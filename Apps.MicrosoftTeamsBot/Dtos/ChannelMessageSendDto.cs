15b43831a7edb1cee0838075de9e25db170365a9
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-02-21 16:40:34 +0200

    File actions

---
+++ Apps.MicrosoftTeamsBot/Dtos/ChannelMessageSendDto.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.MicrosoftTeamsBot.Dtos
+{
+    public class ChannelMessageSendDto
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("attachments")]
+        public List<MessageAttachmentDto> Attachments { get; set; }
+    }
+}
