3d8f849b34f1b8a05b3c6db9411da0e16960faa3
mathijs-bb <mathijs.sonnemans@blackbird.io>
2026-02-13 10:22:30 +0100

    Added proactive token expiry validation

--- Apps.MicrosoftTeamsBot/Auth/OAuth2/OAuth2TokenService.cs
+++ Apps.MicrosoftTeamsBot/Auth/OAuth2/OAuth2TokenService.cs
@@ -1,11 +1,12 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using System.Text.Json;
 
 namespace Apps.MicrosoftTeamsBot.Authorization.OAuth2
 {
-    public class OAuth2TokenService : BaseInvocable, IOAuth2TokenService
+    public class OAuth2TokenService : BaseInvocable, IOAuth2TokenService, ITokenRefreshable
     {
         private const string TokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
         private const string ExpiresAtKeyName = "expires_at";
@@ -16,6 +17,19 @@ namespace Apps.MicrosoftTeamsBot.Authorization.OAuth2
 
         public bool IsRefreshToken(Dictionary<string, string> values)
             => values.TryGetValue(ExpiresAtKeyName, out var expireValue) && DateTime.UtcNow > DateTime.Parse(expireValue);
+
+        public int? GetRefreshTokenExprireInMinutes(Dictionary<string, string> values)
+        {
+            if (!values.TryGetValue(ExpiresAtKeyName, out var expireValue))
+                return null;
+
+            if (!DateTime.TryParse(expireValue, out var expireDate))
+                return null;
+
+            var difference = expireDate - DateTime.UtcNow;
+
+            return (int)difference.TotalMinutes - 5;
+        }
 
         public async Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values, 
             CancellationToken cancellationToken) 
